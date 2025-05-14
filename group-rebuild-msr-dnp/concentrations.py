aad736c597c8e729c202e0cfb992f7cd2d3a12e2
Luke Seifert <LukeSeifert23@gmail.com>
2025-05-14 14:20:58 -0500

    Progressing get all nuclides functionality

---
+++ group-rebuild-msr-dnp/concentrations.py
@@ -0,0 +1,37 @@
+from utils.nuclide_handler import get_all_nuclides
+import numpy as np
+import mypy
+import warnings
+import logging
+import verbose
+
+class Concentrations:
+    """
+    Class to handle the formation of concentrations from data
+    """
+    def __init__(self, input_data: dict) -> None:
+        self.all_nuclides = get_all_nuclides()
+        self.input_data = input_data
+        return None
+    
+    def data_concentrations(self) -> dict[str, list[float]]:
+        """
+        Generate the concentrations of each nuclide from nuclear data
+
+        Returns
+        -------
+        concs : dict[str, list[float]]
+            Dictionary of concentrations for each nuclide
+
+        """
+        raise NotImplementedError("This method should be implemented")
+        return {}
+    
+if __name__ == "__main__":
+    # Example usage
+    input_data = {
+        "nuclide1": [1.0, 2.0, 3.0],
+        "nuclide2": [4.0, 5.0, 6.0]
+    }
+    conc = Concentrations(input_data)
+    print(conc.data_concentrations())
