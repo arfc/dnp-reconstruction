768faa0fd4aa1858291038f0c1aacf26bd45b0ef
Luke Seifert <LukeSeifert23@gmail.com>
2025-05-14 14:20:58 -0500

    Progressing get all nuclides functionality

---
+++ group-rebuild-msr-dnp/utils/nuclide_handler.py
@@ -0,0 +1,13 @@
+
+
+def get_all_nuclides() -> list[str]:
+    """
+    Get all nuclides from the nuclear data
+
+    Returns
+    -------
+    all_nuclides : list[str]
+        List of all nuclides
+    """
+    # This is a placeholder implementation. Replace with actual logic to get nuclides.
+    return ["H-1", "He-4", "Li-6", "Be-9", "B-10", "C-12", "N-14", "O-16"]
