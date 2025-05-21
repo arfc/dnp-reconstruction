63b5423bfa01ca33c724ec72f1be34050d5f945d
Luke Seifert <LukeSeifert23@gmail.com>
2025-05-21 12:49:14 -0500

    Remove openmc data and add input handler

---
+++ group-rebuild-msr-dnp/utils/input_handler.py
@@ -0,0 +1,27 @@
+import json
+
+class InputHandler:
+    def __init__(self, input_path: str) -> None:
+        """
+        This class handles the input files for all processing.
+
+        Parameters
+        ----------
+        input_path : str
+            _description_
+        """
+        self.input_path = input_path
+        return None
+    
+    def read_input(self) -> dict:
+        """
+        Read the input file and return the data as a dictionary.
+
+        Returns
+        -------
+        output : dict
+            Dictionary containing settings and data selections.
+        """
+        with open(self.input_path, 'r') as file:
+            output = json.load(file)
+        return output
