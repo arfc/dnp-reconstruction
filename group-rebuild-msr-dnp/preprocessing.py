532bd280110b4eb1027d4c391c853ba338879dfa
Luke Seifert <LukeSeifert23@gmail.com>
2025-05-21 13:40:39 -0500

    Adding preprocessing

---
+++ group-rebuild-msr-dnp/preprocessing.py
@@ -0,0 +1,22 @@
+from utils.input_handler import InputHandler
+
+class Preprocess:
+    def __init__(self, input_path: str) -> None:
+        """
+        This class handles preprocessing of files into a common format.
+
+        Parameters
+        ----------
+        input_path : str
+            Path to the input file.
+        """
+        self.input_path = input_path
+        self.input_handler = InputHandler(input_path)
+        self.input_data = self.input_handler.read_input()
+        return None
+    
+    def openmc_preprocess(self) -> None:
+        """
+        Processes OpenMC all chain_* files and cross section data using OpenMC
+        """
+        return None
