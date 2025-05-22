b1acf82ec53e8d17e86a919a413e803fd5a6b3c7
Luke Seifert <LukeSeifert23@gmail.com>
2025-05-22 08:55:13 -0500

    Add file and directory handling to utility

--- mosden/utils/csv_handler.py
+++ mosden/utils/csv_handler.py
@@ -1,9 +1,33 @@
 import pandas as pd
+import os
 
 class CSVHandler:
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str, overwrite: bool) -> None:
         self.file_path = file_path
+        self._create_directory() 
+        if not overwrite and self._file_exists():
+            raise FileExistsError(f"File {self.file_path} already exists. Set overwrite=True to overwrite.")
         return None
+    
+    def _create_directory(self) -> None:
+        """
+        Create the directory for the file path if it does not exist.
+        """
+        directory = os.path.dirname(self.file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        return None
+    
+    def _file_exists(self) -> bool:
+        """
+        Check if the file exists at the specified path.
+
+        Returns
+        -------
+        bool
+            True if the file exists, False otherwise.
+        """
+        return os.path.isfile(self.file_path)
 
     def read_csv(self) -> dict[str, dict[str, float]]:
         df = pd.read_csv(self.file_path)
