2b1d122b5060b80506a35baa00196652711dc11f
Luke Seifert <LukeSeifert23@gmail.com>
2025-05-22 11:39:56 -0500

    Partially implemented depletion chain reading

--- mosden/preprocessing.py
+++ mosden/preprocessing.py
@@ -1,6 +1,7 @@
 from utils.input_handler import InputHandler
 from utils.csv_handler import CSVHandler
 import os
+import numpy as np
 
 class Preprocess:
     def __init__(self, input_path: str) -> None:
@@ -12,34 +13,42 @@ class Preprocess:
         input_path : str
             Path to the input file.
         """
-        self.input_path = input_path
-        self.input_handler = InputHandler(input_path)
-        self.input_data = self.input_handler.read_input()
+        self.input_path: str = input_path
+        self.input_handler: InputHandler = InputHandler(input_path)
+        self.input_data: dict = self.input_handler.read_input()
 
-        self.data_dir = self.input_data['data_directory']
-        self.overwrite = self.input_data['overwrite']
-        self.out_dir = self.input_data['output_directory']
+        self.data_dir: str = self.input_data['data_directory']
+        self.overwrite: bool = self.input_data['overwrite']
+        self.out_dir: str = self.input_data['output_directory']
+        self.fissile_targets: list = self.input_data['fissile_targets']
+        self.energy_MeV: float = self.input_data['energy_MeV']
         return None
     
     def openmc_preprocess(self) -> None:
         """
         Processes OpenMC all chain_* files and cross section data using OpenMC
         """
-        self._openmc_chain_preprocess()
+        for fissile in self.fissile_targets:
+            self._openmc_chain_preprocess(fissile)
         return None
     
-    def _openmc_chain_preprocess(self) -> None:
+    def _openmc_chain_preprocess(self, fissile: str) -> None:
         """
         Processes OpenMC all chain_* files
+
+        Parameters
+        ----------
+        fissile : str
+            Name of the fissile target to process.
         """
         for file in os.listdir(self.data_dir + '/chain/'):
-            full_path = os.path.join(self.data_dir + '/chain/', file)
-            file_data = self._process_chain_file(full_path)
-            csv_path = self.data_dir + self.out_dir + file.split('.')[0] + '.csv'
+            full_path: str = os.path.join(self.data_dir + '/chain/', file)
+            file_data: dict[str: dict[str, float]] = self._process_chain_file(full_path, fissile)
+            csv_path: str = self.out_dir + '/' + file.split('.')[0] + '.csv'
             CSVHandler(csv_path, self.overwrite).write_csv(file_data)
         return None
     
-    def _process_chain_file(self, file: str) -> dict[str, dict[str, float]]:
+    def _process_chain_file(self, file: str, fissile: str) -> dict[str, dict[str, float]]:
         """
         Processes a single OpenMC chain file and returns the data as a dictionary.
 
@@ -47,6 +56,8 @@ class Preprocess:
         ----------
         file : str
             Name of the chain file to process.
+        fissile : str
+            Name of the fissile target to process.
 
         Returns
         -------
@@ -54,20 +65,55 @@ class Preprocess:
             Dictionary containing the processed data.
         """
         import openmc.deplete
-        chain = openmc.deplete.Chain.from_xml(file)
+        chain: openmc.deplete.chain = openmc.deplete.Chain.from_xml(file)
         nuclides: list[openmc.deplete.Nuclide] = chain.nuclides
-        for nuc in nuclides:
-            data[nuc.name] = {}
-            data[nuc.name]['half_life'] = nuc.half_life
-            input(nuc.yield_data)
-            input(nuc.yield_energies)
-            help(nuc)
-            input('...')
-        fission_yields: list[dict[str: {str: float}]] = chain.fission_yields
-        # Placeholder for actual processing logic
-        data = {}
-        # Implement the logic to read and process the chain file here
+        nuc_dict: dict[str, int] = chain.nuclide_dict
+        target_index: int = nuc_dict[fissile]
+        target_nuc: openmc.deplete.Nuclide = nuclides[target_index]
+        FY_data: openmc.deplete.FissionYieldDistribution = target_nuc.yield_data
+        energies = FY_data.energies
+        products = FY_data.products
+        FY_chain: dict[str: dict[float, float]] = {}
+        for product in products:
+            FY_chain[product] = {}
+            for energy in energies:
+                FY_chain[product][energy] = FY_data[energy][product]
+
+        fit_FY_chain: dict[str: float] = self._fit_fy_chain(FY_chain, order=1)
+        chain_nucs: list[str] = list(fit_FY_chain.keys())
+        nuclide_halflives: dict[str: float] = {nuc.name: nuc.half_life for nuc in nuclides}
+
+        data: dict[str: dict[str: float]] = dict()
+        for nuc in chain_nucs:
+            data[nuc] = {}
+            data[nuc]['half_life'] = nuclide_halflives[nuc]
+            data[nuc]['FY'] = fit_FY_chain[nuc]
+        
         return data
+    
+    def _fit_fy_chain(self, FY_chain: dict[str: dict[float, float]], order:int=1) -> dict[str: float]:
+        """
+        Fit the fission yield chain data.
+
+        Parameters
+        ----------
+        FY_chain : dict[str: dict[float, float]]
+            Dictionary containing the fission yield chain data.
+        order : int
+            Order of the polynomial fit.
+
+        Returns
+        -------
+        fit_FY_chain : dict[str: float]
+            Dictionary containing the fit fission yield chain data.
+        """
+        fit_FY_chain: dict[str: float] = {}
+        for product in FY_chain:
+            xs = list(FY_chain[product].keys())
+            ys = list(FY_chain[product].values())
+            coeffs = np.polyfit(xs, ys, order)
+            fit_FY_chain[product] = np.polyval(coeffs, self.energy_MeV)
+        return fit_FY_chain
 
 
 preproc = Preprocess('../examples/keepin_1957/pre_input.json')
