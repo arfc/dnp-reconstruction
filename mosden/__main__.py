6a93540662e0a9f5acfef3bb8c4ac41d6001401c
LukeSeifert <lukeseifert23@gmail.com>
2025-07-09 11:06:24 -0500

    Change parse aguments

--- mosden/__main__.py
+++ mosden/__main__.py
@@ -1,7 +1,8 @@
 import argparse
-from . import runme
-from . import preprocessor
-from . import postprocessor  # you'll need to create this
+from . import concentrations
+from . import input_parser
+from . import preprocessing
+from . import postprocessing
 from . import __version__
 
 def main():
@@ -11,17 +12,19 @@ def main():
     group.add_argument("-v", "--version", action="version", version=f"MoSDeN {__version__}")
 
     group.add_argument("-i", "--input", help="Input file for main run")
-    group.add_argument("-p", "--preprocess", help="Input file for preprocessing")
-    group.add_argument("-pp", "--postprocess", help="Input file for postprocessing")
+    group.add_argument("-pre", "--preprocess", help="Input file for preprocessing")
+    group.add_argument("-post", "--postprocess", help="Input file for postprocessing")
 
     args = parser.parse_args()
 
     if args.input:
-        runme.run(args.input)
+        print(args.input)
     elif args.preprocess:
-        preprocessor.preprocess(args.preprocess)
+        print(args.preprocess)
     elif args.postprocess:
-        postprocessor.postprocess(args.postprocess)
+        print(args.postprocess)
+    else:
+        print("No valid option provided. Use -h for help.")
 
 if __name__ == "__main__":
     main()
