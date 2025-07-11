9ebdb3d04508cfbaa42472cf971f5d04f39f3bdf
LukeSeifert <lukeseifert23@gmail.com>
2025-07-11 08:39:33 -0500

    Add endf nfy preprocessing

--- mosden/preprocessing.py
+++ mosden/preprocessing.py
@@ -2,6 +2,8 @@ from mosden.utils.input_handler import InputHandler
 from mosden.utils.csv_handler import CSVHandler
 import os
 import numpy as np
+import re
+from uncertainties import ufloat
 
 class Preprocess:
     def __init__(self, input_path: str) -> None:
@@ -32,6 +34,14 @@ class Preprocess:
             self._openmc_chain_preprocess(fissile)
         return None
     
+    def endf_preprocess(self) -> None:
+        """
+        Processes ENDF data
+        """
+        for fissile in self.fissile_targets:
+            self._endf_nfy_preprocess(fissile)
+        return None
+    
     def _openmc_chain_preprocess(self, fissile: str) -> None:
         """
         Processes OpenMC all chain_* files
@@ -43,13 +53,112 @@ class Preprocess:
         """
         for file in os.listdir(self.data_dir + '/omcchain/'):
             full_path: str = os.path.join(self.data_dir + '/omcchain/', file)
-            file_data: dict[str: dict[str, float]] = self._process_chain_file(full_path, fissile)
+            file_data: dict[str: dict[str: float]] = self._process_chain_file(full_path, fissile)
             path_nuc_energy: str = fissile + '/' + str(self.energy_MeV) + 'MeV'
             csv_path: str = self.out_dir + f'/{path_nuc_energy}/' + file.split('.')[0] + '.csv'
             CSVHandler(csv_path, self.overwrite).write_csv(file_data)
         return None
     
-    def _process_chain_file(self, file: str, fissile: str) -> dict[str, dict[str, float]]:
+    def _endf_nfy_preprocess(self, fissile: str) -> None:
+        """
+        Processes ENDF data for the specified fissile target.
+
+        Parameters
+        ----------
+        fissile : str
+            Name of the fissile target to process.
+        """
+        for file in os.listdir(self.data_dir + '/nfy/'):
+            adjusted_fissile: str = self._endf_fissile_name(fissile)
+            if not file.startswith(f'nfy-{adjusted_fissile}'):
+                continue
+            full_path: str = os.path.join(self.data_dir + '/nfy/', file)
+            file_data : dict[str: dict[str: float]] = self._process_endf_nfy_file(full_path, adjusted_fissile)
+            path_nuc_energy: str = fissile + '/' + str(self.energy_MeV) + 'MeV'
+            csv_path: str = self.out_dir + f'/{path_nuc_energy}/' + file.split('.')[0] + '.csv'
+            CSVHandler(csv_path, self.overwrite).write_csv(file_data) 
+        return None
+    
+    def _endf_fissile_name(self, fissile: str) -> str:
+        """
+        Adjusts the fissile target name for ENDF processing.
+
+        Parameters
+        ----------
+        fissile : str
+            Name of the fissile target to adjust.
+
+        Returns
+        -------
+        adjusted_fissile : str
+            Adjusted fissile target name.
+        """
+        match = re.match(r'([A-Za-z]+)(\d+)', fissile)
+        if not match:
+            raise ValueError(f"Invalid nuclide format: {fissile}")
+
+        symbol, A = match.groups()
+        A = int(A)
+        
+        periodic_table = {
+            'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 'F': 9, 'Ne': 10,
+            'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 'S': 16, 'Cl': 17, 'Ar': 18,
+            'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26,
+            'Co': 27, 'Ni': 28, 'Cu': 29, 'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34,
+            'Br': 35, 'Kr': 36, 'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42,
+            'Tc': 43, 'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50,
+            'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 'Ce': 58,
+            'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 'Tb': 65, 'Dy': 66,
+            'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74,
+            'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82,
+            'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90,
+            'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98,
+            'Es': 99, 'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105,
+            'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 'Cn': 112,
+            'Fl': 114, 'Lv': 116
+        }
+
+        symbol = symbol.capitalize() if len(symbol) == 1 else symbol[0].upper() + symbol[1:].lower()
+
+        Z = periodic_table.get(symbol)
+        if Z is None:
+            raise ValueError(f"Unknown element symbol: {symbol}")
+        
+        return f"{Z:03d}_{symbol}_{A}"
+    
+    def _process_endf_nfy_file(self, file: str, adjusted_fissile: str) -> dict[str, dict[str: float]]:
+        """
+        Processes a single ENDF NFY file and returns the data as a dictionary.
+
+        Parameters
+        ----------
+        file : str
+            Name of the NFY file to process.
+        fissile : str
+            Name of the fissile target to process.
+
+        Returns
+        -------
+        data : dict[str, dict[str: float]]
+            Dictionary containing the processed data.
+        """
+        import openmc.data
+        fpys = openmc.data.FissionProductYields(openmc.data.endf.Evaluation(file))
+        energies = fpys.energies
+        fys = fpys.cumulative
+        endf_nucs: list = list(fys[0].keys())
+        fit_FY_nfy = self._fit_fy_endf(energies, fys)
+
+        data: dict[str: dict[str: float]] = dict()
+        for nuc in endf_nucs:
+            data[nuc] = {}
+            data[nuc]['name'] = nuc
+            data[nuc]['CFY'] = fit_FY_nfy[nuc].n
+            data[nuc][r'$\sigma$ CFY'] = fit_FY_nfy[nuc].s
+        return data
+        
+    
+    def _process_chain_file(self, file: str, fissile: str) -> dict[str, dict[str: float]]:
         """
         Processes a single OpenMC chain file and returns the data as a dictionary.
 
@@ -74,7 +183,7 @@ class Preprocess:
         FY_data: openmc.deplete.FissionYieldDistribution = target_nuc.yield_data
         energies = FY_data.energies
         products = FY_data.products
-        FY_chain: dict[str: dict[float, float]] = {}
+        FY_chain: dict[str: dict[float: float]] = {}
         for product in products:
             FY_chain[product] = {}
             for energy in energies:
@@ -93,11 +202,36 @@ class Preprocess:
             else:
                 data[nuc]['half_life'] = np.inf
             
-            data[nuc]['FY'] = fit_FY_chain[nuc]
+            data[nuc]['IFY'] = fit_FY_chain[nuc]
         
         return data
-    
-    def _fit_fy_chain(self, FY_chain: dict[str: dict[float, float]], order:int=1) -> dict[str: float]:
+
+    def _fit_fy_endf(self, energies: list[float], fys: list[dict[str: ufloat]]) -> dict[str: ufloat]:
+        """
+        Fit the fission yield data from ENDF files.
+        Uses the closest fit, not an interpolation.
+
+        Parameters
+        ----------
+        energies : list[float]
+            List of energy values.
+        ifys : list[dict[str: ufloat]]
+            Dictionary of fission yields or uncertainties at energy indices.
+
+        Returns
+        -------
+        dict[str: ufloat]
+            Dictionary containing the fitted fission yield data.
+        """
+        fit_FY_endf: dict[str: float] = {}
+        endf_nucs: list[str] = list(fys[0].keys())
+        energy_index: int = np.argmin(np.abs(np.array(energies) - self.energy_MeV * 1e6))
+        for i, nuc in enumerate(endf_nucs):
+            fission_yield = fys[energy_index][nuc]
+            fit_FY_endf[nuc] = ufloat(fission_yield.n, fission_yield.s)
+        return fit_FY_endf
+
+    def _fit_fy_chain(self, FY_chain: dict[str: dict[float: float]], order:int=1) -> dict[str: float]:
         """
         Fit the fission yield chain data.
 
@@ -115,15 +249,37 @@ class Preprocess:
         """
         fit_FY_chain: dict[str: float] = {}
         for product in FY_chain:
-            xs = [energy*1e-6 for energy in FY_chain[product].keys()]
-            ys = list(FY_chain[product].values())
-            if order == 1:
-                fit_FY_chain[product] = np.interp(self.energy_MeV, xs, ys)
-            else:
-                coeffs = np.polyfit(xs, ys, order)
-                fit_FY_chain[product] = np.polyval(coeffs, self.energy_MeV)
+            fit_FY_chain[product] = self._energy_fit(energies=list(FY_chain[product].keys()),
+                                                     values=list(FY_chain[product].values()), 
+                                                     order=order)
         return fit_FY_chain
+    
+    def _energy_fit(self, energies: list[float], values: list[float], order: int = 1) -> float:
+        """
+        Fit the energy values to a polynomial of the specified order.
+        Evaluates at `self.energy_MeV`.
+
+        Parameters
+        ----------
+        energies : list[float]
+            List of energy values.
+        values : list[float]
+            List of values corresponding to the energies.
+        order : int
+            Order of the polynomial fit.
+
+        Returns
+        -------
+        fit_value : float
+            The fitted value at `self.energy_MeV`.
+        """
+        xs = [energy*1e-6 for energy in energies]
+        coeffs = np.polyfit(xs, values, order)
+        fit_value = np.polyval(coeffs, self.energy_MeV)
+        return fit_value
+
 
 if __name__ == "__main__":
     preproc = Preprocess('../examples/keepin_1957/pre_input.json')
     preproc.openmc_preprocess()
+    preproc.endf_preprocess()
