b7d0616d1e331d995cae6b1f739e07240bf9293e
LukeSeifert <lukeseifert23@gmail.com>
2025-07-11 09:32:54 -0500

    Cleaned up tests and got working

--- tests/integration/test_preproc.py
+++ tests/integration/test_preproc.py
@@ -9,6 +9,7 @@ def test_openmc_preprocess():
     input_path = Path(__file__).parent / 'inputs' / 'pre_input.json'
     preproc = Preprocess(str(input_path))
     preproc.openmc_preprocess()
+
     # Check if the output directory exists
     output_dir = Path(preproc.out_dir)
     assert output_dir.exists(), f"Output directory {output_dir} does not exist."
@@ -32,7 +33,7 @@ def test_openmc_preprocess():
                     assert '0.123' in content, f"'0.123' not found in {file}."
                     assert '100.0' in content, f"'100.0' not found in {file}."
 
-    shutil.rmtree(output_dir, ignore_errors=True)
+    #shutil.rmtree(output_dir, ignore_errors=True)
     return
 
 def test_endf_preprocess():
@@ -50,21 +51,24 @@ def test_endf_preprocess():
     # Check if the expected files are created
     for fissile in preproc.fissile_targets:
         path_nuc_energy = f"{fissile}/{preproc.energy_MeV}MeV"
-        expected_files = list(output_dir.glob(f"{path_nuc_energy}/*.csv"))
+        expected_files = list(output_dir.glob(f"{path_nuc_energy}/nfy-*.csv"))
         assert expected_files, f"No CSV files found in {path_nuc_energy}."
 
         # Check if the files contain expected data
         for file in expected_files:
             with open(file, 'r') as f:
-                content = f.read()
+                lines = f.readlines()
                 if fissile == 'U235':
-                    assert 'Xe135' in content, f"'Xe135' not found in {file}."
-                    assert '0.065385' in content, f"'0.065385' not found in {file}."
-                    assert '0.000457695' in content, f"'0.000457695' not found in {file}."
+                    line_num = 867
+                    assert 'Xe135' in lines[line_num], f"'Xe135' not found on line {line_num + 1} in {file}."
+                    assert '0.065385' in lines[line_num], f"'0.065385' not found on line {line_num + 1} in {file}."
+                    assert '0.000457695' in lines[line_num], f"'0.000457695' not found on line {line_num + 1} in {file}."
                 elif fissile == 'U238':
-                    assert 'Xe135' in content, f"'Xe135' not found in {file}."
-                    assert '0.123' in content, f"'0.123' not found in {file}."
-                    assert '100.0' in content, f"'100.0' not found in {file}."
+                    line_num = 911
+                    assert 'Xe135' in lines[line_num], f"'Xe135' not found on line {line_num + 1} in {file}."
+                    assert '0.0696758' in lines[line_num], f"'0.0696758' not found on line {line_num + 1} in {file}."
+                    assert '0.000696758' in lines[line_num], f"'0.000696758' not found on line {line_num + 1} in {file}."
+
 
     shutil.rmtree(output_dir, ignore_errors=True)
     return
