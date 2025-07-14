4c8e73a7117b13bb4b90be70c06dafb0565e2f3e
LukeSeifert <lukeseifert23@gmail.com>
2025-07-14 11:37:31 -0500

    Add unit test for concentration init

---
+++ tests/unit/test_concentrations.py
@@ -0,0 +1,15 @@
+from mosden.concentrations import Concentrations
+
+def test_concentrations_init():
+    """
+    Test the initialization of the Concentrations class.
+    """
+
+    input_path = './tests/unit/input/input.json'
+    conc = Concentrations(input_path)
+    assert conc.input_path == input_path, f"Expected input path {input_path}, but got {conc.input_path}"
+    assert conc.output_dir == './tests/unit/output', f"Expected output directory './tests/unit/output', but got {conc.output_dir}"
+    assert conc.energy == 1.0, f"Expected energy 1.0, but got {conc.energy}"
+    assert conc.fissiles == {'U235': 0.8, 'U238': 0.2}, f"Expected fissile targets {{'U235': 0.8, 'U238': 0.2}}, but got {conc.fissiles}"
+
+    return
