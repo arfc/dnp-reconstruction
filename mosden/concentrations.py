56cbf23677832a847d793cde8942606f52706f45
LukeSeifert <lukeseifert23@gmail.com>
2025-07-15 10:54:51 -0500

    Update pathing for data

--- mosden/concentrations.py
+++ mosden/concentrations.py
@@ -21,7 +21,10 @@ class Concentrations:
         self.overwrite: bool = self.input_data['file_options']['overwrite']
         self.FY_file_name: str = self.input_data['data_options']['fission_yield']['type']
         
-
+        self.half_life_dir: str = self.input_data['data_options']['decay_constant']
+        self.cross_section_dir: str = self.input_data['data_options']['cross_section']
+        self.emission_probability_dir: str = self.input_data['data_options']['emission_probability']
+        self.fission_yield_dir: str = self.input_data['data_options']['fission_yield']['data']
         return None
     
     def generate_concentrations(self) -> None:
@@ -45,7 +48,7 @@ class Concentrations:
         concentrations: dict[str: dict[str: ufloat]] = dict()
         all_nucs: set[str] = set()
         for fissile, fraction in self.fissiles.items():
-            CFY_data = self._read_processed_data(fissile)
+            CFY_data = self._read_processed_data(fissile, 'fission_yield')
             for nuclide in CFY_data.keys():
                 concs = ufloat(CFY_data[nuclide]['CFY'], CFY_data[nuclide]['sigma CFY'])
                 concentrations[nuclide] = concentrations.get(nuclide, 0) + concs * fraction
@@ -61,8 +64,8 @@ class Concentrations:
         output_path = Path(self.output_dir) / f"concentrations.csv"
         CSVHandler(output_path, self.overwrite).write_csv(data)
         return
-    
-    def _read_processed_data(self, fissile: str) -> dict[str, dict[str, float]]:
+
+    def _read_processed_data(self, fissile: str, data_type: str) -> dict[str, dict[str, float]]:
         """
         Read the processed data for a given fissile nuclide.
 
@@ -70,14 +73,27 @@ class Concentrations:
         ----------
         fissile : str
             The name of the fissile nuclide.
+        data_type : str
+            The type of data to read (e.g., "fission_yield", "half_life", "cross_section", "emission_probability").
 
         Returns
         -------
         dict
             The processed data for the fissile nuclide.
         """
-        data_path = Path(self.data_path) / fissile / f"{self.energy}MeV" / self.FY_file_name
-        csv_handler = CSVHandler(data_path)
+        if data_type == 'fission_yield':
+            use_path = self.fission_yield_dir
+        elif data_type == 'half_life':
+            use_path = self.half_life_dir
+        elif data_type == 'cross_section':
+            use_path = self.cross_section_dir
+        elif data_type == 'emission_probability':
+            use_path = self.emission_probability_dir
+        else:
+            raise ValueError(f"Unknown data type: {data_type}")
+
+        data_path = Path(self.data_path) / use_path / fissile / f"{self.energy}MeV" / self.FY_file_name
+        csv_handler = CSVHandler(data_path, create=False)
         if not csv_handler._file_exists():
             raise FileNotFoundError(f"Processed data file {data_path} does not exist.") 
         data = csv_handler.read_csv()
