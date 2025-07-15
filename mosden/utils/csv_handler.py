995ec92a76a344b65dc05dd73c0b189d7131a7cc
LukeSeifert <lukeseifert23@gmail.com>
2025-07-15 11:13:26 -0500

    Move file handling and fix bug in data generation

--- mosden/utils/csv_handler.py
+++ mosden/utils/csv_handler.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from uncertainties import ufloat
 
 class CSVHandler:
     def __init__(self, file_path: str, overwrite: bool=False, create=True) -> None:
@@ -29,10 +30,67 @@ class CSVHandler:
         """
         return os.path.isfile(self.file_path)
 
-    def read_csv(self) -> dict[str, dict[str, float]]:
+    def read_csv(self, raw_iaea=False) -> dict[str, dict[str, float]]:
+        if raw_iaea:
+            return self._read_iaea_csv()
         df = pd.read_csv(self.file_path, index_col=0)
         data = df.to_dict(orient='index')
         return data
+    
+    def _read_iaea_csv(self) -> dict[str, dict[str, float]]:
+        """
+        Read the IAEA CSV file and return the data as a dictionary.
+
+        Returns
+        -------
+        dict
+            The data read from the IAEA CSV file.
+        """
+        data: dict[str, dict[str, float]] = {}
+        df = pd.read_csv(self.file_path, header=1)
+        for _, row in df.iterrows():
+            iaea_nuc = row['nucid']
+            nuc = self._iaea_to_mosden_nuc(iaea_nuc)
+            half_life = row[' T1/2 [s] ']
+            half_life_uncertainty = row[' D T1/2 [s]']
+            if row['D pn1'] < 0:
+                row['D pn1'] = 0
+            if row['D pn2 '] < 0:
+                row['D pn2 '] = 0
+            if row['D pn3'] < 0:
+                row['D pn3'] = 0
+            P1 = ufloat(row[' pn1 % ']/100, row['D pn1']/100)
+            P2 = ufloat(row[' pn2 % ']/100, row['D pn2 ']/100)
+            P3 = ufloat(row[' pn3 % ']/100, row['D pn3']/100)
+            prob_beta = ufloat(row['  beta- %']/100, row[' D beta-']/100)
+            emission_prob = (1*P1 + 2*P2 + 3*P3) * prob_beta
+            data[nuc] = {}
+            data[nuc]['half_life'] = half_life
+            data[nuc]['sigma half_life'] = half_life_uncertainty
+            data[nuc]['emission probability'] = emission_prob.n
+            data[nuc]['sigma emission probability'] = emission_prob.s
+        return data
+    
+    def _iaea_to_mosden_nuc(self, iaea_nuc: str) -> str:
+        """
+        Converts IAEA nuclide format to MoSDeN format.
+
+        Parameters
+        ----------
+        iaea_nuc : str
+            IAEA nuclide identifier.
+
+        Returns
+        -------
+        str
+            MoSDeN formatted nuclide identifier.
+        """
+        i = 0
+        while i < len(iaea_nuc) and iaea_nuc[i].isdigit():
+            i += 1
+        mass = iaea_nuc[:i]
+        element = iaea_nuc[i:].capitalize()
+        return f"{element}{mass}"
 
     def write_csv(self, data: dict[str, dict[str, float]]) -> None:
         if not self.overwrite and self._file_exists():
