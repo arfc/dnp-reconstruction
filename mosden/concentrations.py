0795163352eefa221070e6f0020ffef8042b78a1
LukeSeifert <lukeseifert23@gmail.com>
2025-07-16 09:17:47 -0500

    Add base class and restructure input files

--- mosden/concentrations.py
+++ mosden/concentrations.py
@@ -3,15 +3,14 @@ from mosden.utils.input_handler import InputHandler
 from mosden.utils.csv_handler import CSVHandler
 from pathlib import Path
 from uncertainties import ufloat
+from mosden.base import BaseClass
 
-class Concentrations:
+class Concentrations(BaseClass):
     """
     Class to handle the formation of concentrations from data
     """
     def __init__(self, input_path: str) -> None:
-        self.input_path: str = input_path
-        self.input_handler: InputHandler = InputHandler(input_path)
-        self.input_data: dict = self.input_handler.read_input()
+        super().__init__(input_path)
         self.output_dir: str = self.input_data['file_options']['output_dir']
 
         self.model_method: str = self.input_data['modeling_options']['concentration_handling']
@@ -19,7 +18,6 @@ class Concentrations:
         self.data_path: str = self.input_data['file_options']['processed_data_dir']
         self.energy: float = self.input_data['data_options']['energy_MeV']
         self.overwrite: bool = self.input_data['file_options']['overwrite']
-        self.FY_file_name: str = self.input_data['data_options']['fission_yield']['type']
 
         self.reprocessing: dict[str: float] = self.input_data['modeling_options']['reprocessing']
         self.reprocess: bool = (sum(self.reprocessing.values()) > 0)
@@ -28,11 +26,7 @@ class Concentrations:
         self.t_ex: float = self.input_data['modeling_options']['excore_s']
         self.t_net: float = self.input_data['modeling_options']['net_irrad_s']
         self.irrad_type: str = self.input_data['modeling_options']['irrad_type']
-        
-        self.half_life_dir: str = self.input_data['data_options']['decay_constant']
-        self.cross_section_dir: str = self.input_data['data_options']['cross_section']
-        self.emission_probability_dir: str = self.input_data['data_options']['emission_probability']
-        self.fission_yield_dir: str = self.input_data['data_options']['fission_yield']['data']
+
         return None
     
     def generate_concentrations(self) -> None:
@@ -77,39 +71,6 @@ class Concentrations:
         CSVHandler(output_path, self.overwrite).write_csv(data)
         return
 
-    def _read_processed_data(self, fissile: str, data_type: str) -> dict[str, dict[str, float]]:
-        """
-        Read the processed data for a given fissile nuclide.
-
-        Parameters
-        ----------
-        fissile : str
-            The name of the fissile nuclide.
-        data_type : str
-            The type of data to read (e.g., "fission_yield", "half_life", "cross_section", "emission_probability").
-
-        Returns
-        -------
-        dict
-            The processed data for the fissile nuclide.
-        """
-        if data_type == 'fission_yield':
-            use_path = self.fission_yield_dir
-        elif data_type == 'half_life':
-            use_path = self.half_life_dir
-        elif data_type == 'cross_section':
-            use_path = self.cross_section_dir
-        elif data_type == 'emission_probability':
-            use_path = self.emission_probability_dir
-        else:
-            raise ValueError(f"Unknown data type: {data_type}")
-
-        data_path = Path(self.data_path) / use_path / fissile / f"{self.energy}MeV" / self.FY_file_name
-        csv_handler = CSVHandler(data_path, create=False)
-        if not csv_handler._file_exists():
-            raise FileNotFoundError(f"Processed data file {data_path} does not exist.") 
-        data = csv_handler.read_csv()
-        return data
 
 
 
--- mosden/countrate.py
+++ mosden/countrate.py
@@ -1,5 +1,6 @@
 from mosden.utils.input_handler import InputHandler
 from mosden.utils.csv_handler import CSVHandler
+import os
 
 
 class CountRate:
@@ -10,8 +11,28 @@ class CountRate:
         self.input_path: str = input_path
         self.input_handler: InputHandler = InputHandler(input_path)
         self.input_data: dict = self.input_handler.read_input()
+        self.data_dir: str = self.input_data['file_options']['processed_data_dir']
         self.output_dir: str = self.input_data['file_options']['output_dir']
+
+        self.parent_feed: bool = self.input_data['modeling_options']['parent_feeding']
+        self.num_times: int = self.input_data['modeling_options']['num_decay_times']
+        self.decay_time: float = self.input_data['modeling_options']['decay_time']
         
+        self.half_life_dir: str = self.input_data['data_options']['decay_constant']
+        self.cross_section_dir: str = self.input_data['data_options']['cross_section']
+        self.emission_probability_dir: str = self.input_data['data_options']['emission_probability']
+        self.fission_yield_dir: str = self.input_data['data_options']['fission_yield']['data'] 
         return None
     
-    
+    def calculate_count_rate(self) -> None:
+        """
+        Calculate the delayed neutron count rate from
+        concentrations using various methods
+        """
+        # Get data (conc.csv and emission probability processed data)
+        # Calculate count rate (method (depletion linear interp parent feeding or not), decay time, decay time steps, depletion time steps)
+        # Generate CSV with count rate over time
+        emission_prob_path: str = os.path.join(self.data_dir, self.emission_probability_dir, 'eval.csv')
+        emission_prob_data = CSVHandler(emission_prob_path, create=False)
+
+        return
