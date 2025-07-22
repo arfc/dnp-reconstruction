98a9aef920d7aa672f24562837c520f5382ddfbf
LukeSeifert <lukeseifert23@gmail.com>
2025-07-22 08:52:32 -0500

    Update countrate and add integration test

---
+++ tests/integration/test_countrate.py
@@ -0,0 +1,34 @@
+from mosden.countrate import CountRate
+from pathlib import Path
+from mosden.utils.csv_handler import CSVHandler
+import numpy as np
+import pytest
+
+@pytest.mark.parametrize("input_path, reference_output_path", [
+    ("tests/integration/test-data/input1.json", "tests/integration/test-data/reference/test1")
+] )
+def test_calculate_count_rate(input_path, reference_output_path):
+    """
+    Test the count rate calculation.
+    """
+    countrate = CountRate(input_path)
+    countrate.data_dir = reference_output_path
+    
+    # Generate count rate
+    countrate.calculate_count_rate()
+    
+    # Check if output file is created
+    output_path = Path(countrate.output_dir) / "count_rate.csv"
+    assert output_path.exists(), f"Output file {output_path} does not exist."
+
+    # Check if the output file has the expected content
+    data = CSVHandler(output_path).read_countrate_csv()
+    assert data, "Output file is empty."
+
+    # Check all concentrations
+    reference_path = Path(reference_output_path) / "count_rate.csv"
+    reference_data = CSVHandler(reference_path).read_countrate_csv()
+
+    assert data == reference_data, "Output count rate does not match the expected reference count rate."
+
+    return
