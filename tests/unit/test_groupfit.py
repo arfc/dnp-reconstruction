c814955bdd61992573db08a6256639da3d5c2d4c
LukeSeifert <lukeseifert23@gmail.com>
2025-07-23 10:05:47 -0500

    Fix groupfit by using more compute and add simple unit test

--- tests/unit/test_groupfit.py
+++ tests/unit/test_groupfit.py
@@ -1,4 +1,5 @@
 from mosden.groupfit import Grouper
+import numpy as np
 
 def test_grouper_init():
     """
@@ -12,3 +13,40 @@ def test_grouper_init():
     assert countrate.fissiles == {'U235': 0.8, 'U238': 0.2}, f"Expected fissile targets {{'U235': 0.8, 'U238': 0.2}}, but got {countrate.fissiles}"
 
     return
+
+def test_grouper_pulse_fitting():
+    """
+    Test the nonlinear least squares fitting for a pulse case
+    """
+    input_path = './tests/unit/input/input.json'
+    grouper = Grouper(input_path)
+    count_data: dict[str: np.ndarray[float]] = dict()
+
+    half_lives = [60, 50, 40, 30, 20, 10]
+    yields = [0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+    times = np.linspace(0, 600, 100)
+    counts = np.zeros(len(times))
+    for group in range(len(half_lives)):
+        lam = np.log(2) / half_lives[group]
+        a = yields[group]
+        counts += a * lam * np.exp(-lam * times)
+    
+    parameters = yields + half_lives
+    func_counts = grouper._pulse_fit_function(times, parameters)
+    assert np.isclose(func_counts, counts).all(), 'Pulse counts mismatch between hand calculation and function evaluation'
+
+    grouper.irrad_type = 'pulse'
+    grouper.num_groups = 6
+    count_data['times'] = times
+    count_data['counts'] = counts
+    count_data['sigma counts'] = np.zeros(len(counts))
+    data = grouper._nonlinear_least_squares(count_data=count_data)
+    test_yields = [data[key]['yield'] for key in data.keys()]
+    test_half_lives = [data[key]['half_life'] for key in data.keys()]
+    test_yields.sort(reverse=True)
+    test_half_lives.sort(reverse=True)
+    for group in range(grouper.num_groups):
+        assert np.isclose(test_yields[group], yields[group]), f'Group {group+1} yields - {test_yields[group]=} != {yields[group]=}'
+        assert np.isclose(test_half_lives[group], half_lives[group]), f'Group {group+1} half lives mismatch - {test_yields[group]=} != {half_lives[group]=}'
+    
+    
