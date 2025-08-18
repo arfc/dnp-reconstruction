88859655a0eb19eb199c81d069657ef57a30c044
LukeSeifert <lukeseifert23@gmail.com>
2025-08-18 14:41:47 -0500

    Fix typo, non-zero ex-core time functional

--- mosden/concentrations.py
+++ mosden/concentrations.py
@@ -35,7 +35,7 @@ class Concentrations(BaseClass):
         self.t_net: float = modeling_options.get('net_irrad_s', 0.0)
         self.irrad_type: str = modeling_options.get('irrad_type', 'saturation')
         self.f_in: float = self.t_in / (self.t_in + self.t_ex)
-        self.f_ex: float = self.t_ex / (self.t_net + self.t_ex)
+        self.f_ex: float = self.t_ex / (self.t_in + self.t_ex)
 
         self.repr_scale = 0.0
         if 'incore' in self.reprocess_locations:
@@ -55,9 +55,6 @@ class Concentrations(BaseClass):
         start = time()
         data: dict[str: dict[str: float]] = dict()
         if self.model_method == 'CFY':
-            if self.t_ex > 0.0:
-                raise NotImplementedError(
-                    'Excore residence not available for CFY')
             if self.irrad_type != 'saturation':
                 self.logger.error(
                     'CFY is intended for a saturation irradiation')
